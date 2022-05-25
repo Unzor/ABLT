cc377ac8f05649752262161dff3877fbdf15548c
Unzor <71789103+Unzor@users.noreply.github.com>
2022-05-25 17:27:48 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,19 +1,36 @@
 const express = require('express');
+const fs = require("fs");
 
 const app = express();
+app.use(express.static(__dirname));
 
-app.get('/ablt/attach/:code', (req, res) => {
-  function ABLT_FN(name, fn){
+function ABLT_FN(fn){
 	var fne;
-	app.get("/ablt/run/" + name, (req, res) => {
+	app.get("/ablt/run/" + fn.name, async (req, res) => {
 		if (req.query.args) {
 			var args = atob(req.query.args).split(",");
-			res.send(fn(...args))
+      if (fn.constructor.name == "AsyncFunction") {
+			  res.send(await fn(...args))
+      } else {
+        res.send(fn(...args))
+      }
 		} else {
-			fn()
+			if (fn.constructor.name == "AsyncFunction") {
+			  res.send(await fn(...args))
+      } else {
+        res.send(fn(...args))
+      }
 		}
 	})
   }
+
+if (fs.existsSync("preload.json")) {
+  var preload = JSON.parse(fs.readFileSync("preload.json").toString());
+  preload.files.forEach(function(file){
+    eval(fs.readFileSync(file).toString())
+  })
+}
+app.get('/ablt/attach/:code', (req, res) => {
   eval(atob(req.params.code))
   res.send("attached")
 });
