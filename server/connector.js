bb56a2e9977813a71fb8af9034f712ec1c7aa6c8
Unzor <71789103+Unzor@users.noreply.github.com>
2022-05-25 08:13:26 -0500

    Delete connector.js

--- server/connector.js
+++
@@ -1,6 +0,0 @@
-var fs = require("fs");
-var filename = process.argv[2];
-var file = fs.readFileSync(filename).toString();
-fetch("http://localhost:3000/ablt/attach/" + btoa(file)).then(x => x.text()).then(x => {
-	console.log(x);
-})
